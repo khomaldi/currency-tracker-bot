335895280a37bb7253dbdb726c4672fb575321ec
Georgii <khomaldi@yandex.ru>
2022-05-26 18:47:37 +0300

    add ability to send message to telegram chat id

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,12 +4,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class Controller
+class Controller extends AbstractController
 {
 
     public function index(): JsonResponse
     {
+        $msg = $this->prepareMessage($this->getBankCurrencyRates(), $this->getStockCurrencyRates());
+
+        $this->sendMessageToChat($msg);
+
         return new JsonResponse(
             [
                 'bank'  => $this->getBankCurrencyRates(),
@@ -36,14 +41,49 @@ class Controller
             }
         );
 
-        $currencyRates = array_map(static function ($value) {
-            return [
+        foreach ($currencyRates as $key => $value) {
+            $currencyRates[$value['fromCurrency']['name']] = [
                 'name' => $value['fromCurrency']['name'],
                 'sell' => $value['buy'],
                 'buy'  => $value['sell'],
             ];
-        }, $currencyRates);
 
-        return array_values($currencyRates);
+            unset($currencyRates[$key]);
+        }
+
+        return $currencyRates;
+    }
+
+    private function prepareMessage(array $bankCurrencyRates, ?array $stockCurrencyRates = null): string
+    {
+        date_default_timezone_set('Europe/Moscow');
+
+        $now = date('d.m.Y H:i:s') . ' Moscow';
+
+        $usdString = "{$bankCurrencyRates['USD']['sell']}   {$bankCurrencyRates['USD']['buy']}";
+        $eurString = "{$bankCurrencyRates['EUR']['sell']}   {$bankCurrencyRates['EUR']['buy']}";
+        $gbpString = "{$bankCurrencyRates['GBP']['sell']}   {$bankCurrencyRates['GBP']['buy']}";
+
+        return "<pre>{$now}\n\n<b>USD</b> {$usdString}\n<b>EUR</b> {$eurString}\n<b>GBP</b> {$gbpString}\n</pre>";
+    }
+
+    function sendMessageToChat(string $text)
+    {
+        $client = HttpClient::create();
+
+        $chatId = $this->getParameter('app.telegram_chat_id');
+        $botToken = $this->getParameter('app.telegram_bot_token');
+        $response = $client->request(
+            'POST',
+            "https://api.telegram.org/bot{$botToken}/sendMessage",
+            [
+                'json' => [
+                    'chat_id'    => $chatId,
+                    'text'       => $text,
+                    'parse_mode' => 'html',
+                ],
+            ]
+        );
+
     }
 }
